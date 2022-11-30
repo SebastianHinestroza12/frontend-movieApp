560e11e1cd3bb6a811fef7aca11b805be3985c0a
SebastianHinestroza12 <menasebastian1356@americana.edu.co>
2022-11-29 20:19:46 -0500

    add configuration chakra ui

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./Redux/Store/index";
+import { theme } from "./theme";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
@@ -10,9 +12,12 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ChakraProvider>
     ,
   </React.StrictMode>
 );
